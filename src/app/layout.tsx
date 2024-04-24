ebde9e4d3d63aab8e955bd5173151a4733bd0bf6
DESKTOP-8SD3V7L\User <farizsalman14@gmail.com>
2024-04-24 12:50:17 +0700

    UPDATE - Core - Config - Pages - Package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
+import { NavBrand, NavLink, NavLinkProps } from "@/components/ui/nav.ui";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeButton } from "@/components/ui/theme.ui";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+const ibm_plex_mono = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400"], style: ["normal"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +19,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const NavLinkData: NavLinkProps[] = [
+    { url: "about", title: "About" },
+    { url: "", title: "Education" },
+    { url: "", title: "Skill" },
+    { url: "", title: "Contact" },
+    { url: "", title: "Blog" },
+  ]
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ibm_plex_mono.className + ' flex flex-col max-w-screen-lg mx-auto'}>
+        <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
+          <Navbar NavLinkData={NavLinkData} />
+          <div className="flex justify-center w-full">
+            {children}
+            <ThemeButton />
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
