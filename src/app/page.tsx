425cc3e7f1d8ff4ab1c263cd8049a42f6ca64ca9
DESKTOP-8SD3V7L\User <farizsalman14@gmail.com>
2024-04-24 13:20:08 +0700

    UPDATE - Fix Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import { BiLogoTypescript, BiLogoJavascript, BiLogoPython, BiLogoGoLang, BiLogoP
 
 export default function Home() {
 
-  return (<section className="mt-16" id="about">
-    <div className="flex flex-row justify-between bg-stone-600/60 dark:bg-stone-400 max-w-screen-md w-full rounded-md py-5 px-4 shadow-md mb-3">
-      <div className="flex flex-col text-stone-100 dark:text-stone-800 w-[40%]">
+  return (<section className="mt-16 px-10" id="about">
+    <div className="flex flex-col md:flex-row justify-between bg-stone-600/60 dark:bg-stone-400 md:max-w-screen-md w-full rounded-md py-5 px-4 shadow-md mb-3">
+      <div className="flex flex-col text-stone-100 dark:text-stone-800 md:w-[40%] w-full">
         <div className="text-sm font-bold tracking-widest leading-5">Muhammad Salman Al Farizi</div>
         <div className="text-xs tracking-wide leading-5">Fullstack Developer</div>
       </div>
-      <div className="w-[60%] text-justify text-sm text-stone-100 dark:text-stone-800">
+      <div className="md:w-[60%] mt-2 md:mt-0 w-full text-justify text-sm text-stone-100 dark:text-stone-800">
         Over the last three odd years i&apos;ve had the pleasure of working with some great companies.
         working side by side to design and develop new apps and improve upon existing products. [Go, Ts, Js, PHP, Py, etc.]
       </div>
     </div>
     <div className="flex flex-row justify-center bg-stone-600/60 dark:bg-stone-400 max-w-screen-md w-full rounded-md py-5 px-4 shadow-md mb-3">
-      <div className="text-8xl flex flex-row justify-between w-full text-stone-100 dark:text-stone-800">
+      <div className="text-8xl flex flex-wrap justify-evenly w-full text-stone-100 dark:text-stone-800">
         <BiLogoGoLang />
         <BiLogoJavascript />
         <BiLogoTypescript />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,10 +9,10 @@ export function Navbar({ NavLinkData }: NavProps) {
   let i: number = 0;
   return (
     <>
-      <nav className="flex justify-center align-middle py-3 fixed max-w-screen-lg w-full">
+      <nav className="flex justify-center align-middle py-3 fixed max-w-screen-lg w-full px-10">
         <div className="flex justify-between align-middle max-w-screen-md w-full bg-stone-600/60 dark:bg-stone-400 py-3 px-6 rounded-md shadow-lg">
           <NavBrand title="Alyvazen" />
-          <ul className="flex justify-end align-middle gap-3">
+          <ul className="hidden md:flex justify-end align-middle gap-3">
             {NavLinkData.map(n => (
               <NavLink url={n.url} title={n.title} key={i++} />
             ))}
